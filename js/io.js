3e5e0070d2f4c078e66c7c7ca4d9d83590994327
AMAID <8609546@qq.com>
2014-02-22 00:57:36 +0800

    测试大小

---
+++ js/io.js
@@ -0,0 +1,10 @@
+﻿function jsonp(url, callback) {
+    var callbackName = 'jsonp' + Date.now()
+    var script = document.createElement('script')
+    script.src = url + '?callback=' + callbackName
+    window[callbackName] = function (data) {
+        document.body.removeChild(script);
+        callback(data);
+    }
+    document.body.appendChild(script)
+}
