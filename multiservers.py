d82647961add000e0e37c25f2b9da384564a2793
George Chung <Gerhut@GMail.com>
2014-02-22 00:07:44 +0800

    multiserver only listen to localhost

--- multiservers.py
+++ multiservers.py
@@ -4,8 +4,9 @@ from thread import start_new_thread
 __all__ = ['start']
 
 def start_http_server(port, handler):
-    HTTPServer(("", port), handler).serve_forever()
+    HTTPServer(("127.0.0.1", port), handler).serve_forever()
 
 def start(ports, handler):
     for port in ports:
         start_new_thread(start_http_server, (port, handler))
+        print 'Start HTTP server on port %d' % (port,)
