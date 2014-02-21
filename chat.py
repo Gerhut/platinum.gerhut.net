693e3d12481fd2d908de04bbd4e462e26a164f1e
George Chung <Gerhut@GMail.com>
2014-02-22 00:07:52 +0800

    debug chat service

--- chat.py
+++ chat.py
@@ -1,12 +1,13 @@
-from time import time
+from time import time, strftime, localtime
 
 __all__ = ['content', 'add']
 
 content = ''
 chats = []
 
-def add(name, content):
+def add(name, says):
+    global chats, content
     chats.insert(0, '(%s)[%s]%s' % (strftime('%H:%M:%S', localtime(time()))
-        , name, content))
+        , name, says))
     chats = chats[:50]
     content = '\n'.join(chats)
