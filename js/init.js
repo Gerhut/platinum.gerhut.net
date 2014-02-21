7a981e1e9f546be9206710fce0a5ad4e2cea37df
AMAID <8609546@qq.com>
2014-02-22 04:01:58 +0800

    完成聊天和图形输出界面

---
+++ js/init.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    var name = 'user';//测试用户名
+    var host = window.location.host, gamehost = 'pokemod.gerhut.net';
+    var version, port;
+    switch (host) {
+        case 'platium.gerhut.net': version = 'platium'; port = '4930'; break;
+        case 'heartgold.gerhut.net': version = 'heartgold'; port = '2510'; break;
+        default: version = 'platium'; version = 'platium'; port = '4930';
+    }
+
+    var urlImg = 'http://' + gamehost + ':' + port, urlChat = 'http://' + gamehost + ':' + port + '/chat', urlKeys = 'http://' + gamehost + ':' + port + '/keys';
+    var timeout = -1
+    var gamescreen = document.getElementById('imgGamescreen'), chatlist = document.getElementById('chatlist'), message = document.getElementById('txtMessage');
+
+    window.load = function () {
+
+        if (window.timeout > -1)
+            clearTimeout(timeout)
+        timeout = setTimeout(refresh, 100)
+    }
+
+    function refresh() {
+        gamescreen.src = urlImg;
+        jsonp(urlChat, function (data) {
+            chatlist.innerText = data;
+        })
+    }
+    window.talk = function () {
+        if (message.value != '' && message.value.length <= 40) {
+            jsonp(urlChat + '/' + encodeURIComponent(name) + '/' + encodeURIComponent(txtMessage.value), function send(data) {
+                return;
+            })
+            message.value = ''
+        }
+    }
+
+})()
