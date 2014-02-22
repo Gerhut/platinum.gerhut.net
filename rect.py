5c3bb6567bce54f42a221a879750789204c6cad8
George Chung <Gerhut@GMail.com>
2014-02-22 11:22:25 +0800

    spilt screenshot into screenshot and get rect

---
+++ rect.py
@@ -0,0 +1,35 @@
+import ctypes
+from time import sleep
+
+__all__ = ['get']
+
+class RECT(ctypes.Structure):
+    _fields_ = [('left', ctypes.c_long),
+                ('top', ctypes.c_long),
+                ('width', ctypes.c_long),
+                ('height', ctypes.c_long)]
+    def bbox(self):
+        yield self.left
+        yield self.top
+        yield self.left + self.width
+        yield self.top + self.height
+
+_title = None
+_rect = None
+
+def get(title = None):
+    global _title, _rect
+    if title != _title:
+        while True:
+            hwnd = ctypes.windll.user32.FindWindowA(None, title)
+            if hwnd != 0:
+                _rect = RECT()
+                ctypes.windll.user32.ShowWindow(hwnd, 9) # SW_RESTORE
+                ctypes.windll.user32.GetClientRect(hwnd, ctypes.byref(_rect))
+                ctypes.windll.user32.ClientToScreen(hwnd, ctypes.byref(_rect))
+                _rect = tuple(_rect.bbox())
+                _title = title
+                break
+            print 'Cannot find the window.'
+            sleep(delay)
+    return _rect
--- screenshot.py
+++ screenshot.py
@@ -1,30 +1,16 @@
-#coding:utf-8
 
-import ctypes
 from StringIO import StringIO
 from time import sleep
 from ImageGrab import grab
+import rect
 
 __all__ = ['image', 'delay', 'start']
 
 image = ''
 delay = .1
 
-rect = None
-
-class RECT(ctypes.Structure):
-    _fields_ = [('left', ctypes.c_long),
-                ('top', ctypes.c_long),
-                ('width', ctypes.c_long),
-                ('height', ctypes.c_long)]
-    def bbox(self):
-        yield self.left
-        yield self.top
-        yield self.left + self.width
-        yield self.top + self.height
-
-def shotScreen():
-    global image, rect
+def shotScreen(rect):
+    global image
     print 'Start screenshotting.'
     while True:
         stringIO = StringIO()
@@ -38,18 +24,4 @@ def shotScreen():
         sleep(delay)
 
 def start(title):
-    global rect
-    while True:
-        hwnd = ctypes.windll.user32.FindWindowA(None, title)
-        if hwnd != 0:
-            rect = RECT()
-            ctypes.windll.user32.ShowWindow(hwnd, 9) # SW_RESTORE
-            ctypes.windll.user32.GetClientRect(hwnd, ctypes.byref(rect))
-            ctypes.windll.user32.ClientToScreen(hwnd, ctypes.byref(rect))
-            rect = tuple(rect.bbox())
-            break
-        print 'Cannot find the window to grab.'
-        sleep(delay)
-        continue
-
-    shotScreen()
+    shotScreen(rect.get(title))
