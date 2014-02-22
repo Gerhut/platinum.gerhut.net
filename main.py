15156933502eebb7d34f93ef76afcaea3fb9153d
George Chung <Gerhut@GMail.com>
2014-02-22 12:16:41 +0800

    add inputs service to main

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from BaseHTTPServer import BaseHTTPRequestHandler
 from urllib import unquote
 from json import dump
 from os import system
+from thread import start_new_thread
+from sys import exc_info
 
 import screenshot
 import multiservers
 import chat
+import inputs
 
 class MultiPokeHandler(BaseHTTPRequestHandler):
     def log_message(*args, **kwargs):
@@ -30,28 +33,75 @@ class MultiPokeHandler(BaseHTTPRequestHandler):
         dump(chat.content, self.wfile)
         self.wfile.write(')')
         self.wfile.close()
+    def send_inputs(self, callback_name):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/javascript')
+        self.end_headers()
+        self.wfile.write(callback_name)
+        self.wfile.write('(')
+        dump(inputs.content, self.wfile)
+        self.wfile.write(')')
+        self.wfile.close()
     def do_GET(self):
-        if self.path == '/':
-            self.send_screen_shot()
-        elif self.path.startswith('/chat'):
-            path = self.path[5:]
+        try:
+            if self.path.startswith('/?'):
+                self.send_screen_shot()
+            elif self.path.startswith('/chat'):
+                path = self.path[5:]
 
-            callback_index = path.find('?callback=')
-            if callback_index == -1:
-                return self.send_error(400, 'Callback required'.)
-            callback_name = path[callback_index + 10:]
+                callback_index = path.find('?callback=')
+                if callback_index == -1:
+                    return self.send_error(400, 'Callback required'.)
+                callback_name = path[callback_index + 10:]
 
-            path = path[:callback_index]
-            
-            if path.startswith('/'):
-                components = path.split('/')
-                if len(components) == 3:
-                    chat.add(unquote(components[1]), unquote(components[2]))
+                path = path[:callback_index]
+                
+                if path.startswith('/'):
+                    components = path.split('/')
+                    if len(components) == 3:
+                        chat.add(unquote(components[1]), unquote(components[2]))
 
-            self.send_chat(callback_name)
-        else:
+                self.send_chat(callback_name)
+            elif self.path.startswith('/input'):
+                path = self.path[6:]
+
+                callback_index = path.find('?callback=')
+                if callback_index == -1:
+                    return self.send_error(400, 'Callback required'.)
+                callback_name = path[callback_index + 10:]
+
+                path = path[:callback_index]
+
+                if path.startswith('/'):
+                    components = path.split('/')
+                    if len(components) == 4:
+                        if components[2] == 'key':
+                            start_new_thread(inputs.key, (unquote(components[1]), int(components[3])))
+                        elif components[2] == 'mouse':
+                            (x, y) = components[3].split(',')
+                            start_new_thread(inputs.mouse, (unquote(components[1]), int(x), int(y)))
+                self.send_chat(callback_name)
+            else:
+                self.send_error(400, 'Invalid request.')
+        except:
+            print exc_info()
             self.send_error(400, 'Invalid request.')
 
 system('run')
+inputs.available_mouse = rect.get('No$gba Emulator ')
+inputs.available_keys = {
+    0x57: '上',
+    0x53: '下',
+    0x41: '左',
+    0x44: '右',
+    0x5A: 'Ａ',
+    0x58: 'Ｂ',
+#    0x52: '选择',
+    0x46: '开始',
+    0x51: 'Ｌ',
+    0x45: 'Ｒ',
+    0x43: 'Ｘ',
+    0x56: 'Ｙ',
+}
 multiservers.start(range(4931, 4940), MultiPokeHandler)
 screenshot.start('No$gba Emulator ')
