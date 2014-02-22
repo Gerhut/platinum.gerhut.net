9844c398f82d87ec9b7b8ad228680ddf8ae7520a
AMAID <8609546@qq.com>
2014-02-22 22:14:02 +0800

    接口变更

--- js/init.js
+++ js/init.js
@@ -9,11 +9,14 @@
     }
 
     var urlImg = 'http://' + gamehost + '/' + version
-      , urlChat = 'http://' + gamehost + '/' + version + '/chat'
+      , urlInfo = 'http://' + gamehost + '/' + version + '/info'
       , urlKeys = 'http://' + gamehost + '/' + version + '/input/gerhut/key'
       , urlMouse = 'http://' + gamehost + '/' + version + '/input/gerhut/mouse';
     var timeout = -1
-    var gamescreen = document.getElementById('imgGamescreen'), chatlist = document.getElementById('chatlist'), message = document.getElementById('txtMessage');
+    var gamescreen = document.getElementById('imgGamescreen')
+        , chatlist = document.getElementById('chatlist')
+        , inputlist = document.getElementById('inputlist')
+        , message = document.getElementById('txtMessage');
 
     window.sendkey = function (code) {
         jsonp(urlKeys + '/' + code, function (data) {
@@ -26,14 +29,15 @@
     }
 
     window.refreshChat = function() {
-        jsonp(urlChat, function (data) {
-            chatlist.innerHTML = data;
+        jsonp(urlInfo, function (chatData, inputData) {
+            chatlist.innerHTML = chatData;
+            inputlist.innerHTML = inputData;
             setTimeout(window.refreshChat, 500);
         })
     }
     window.talk = function () {
         if (message.value != '' && message.value.length <= 40) {
-            jsonp(urlChat + '/' + encodeURIComponent(name) + '/' + encodeURIComponent(txtMessage.value), function send(data) {
+            jsonp(urlChat + '/' + encodeURIComponent(name) + '/' + encodeURIComponent(txtMessage.value),function send(data) {
                 return;
             })
             message.value = ''
